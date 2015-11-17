b10970e25287d7f10005894ebc06706a6399c5e0
Kyle Lee <kiloloco@me.com>
2015-11-17 12:54:21 -0800

    Finished stack view layout of details VC

--- Pokedex/PokemonDetailVC.swift
+++ Pokedex/PokemonDetailVC.swift
@@ -11,9 +11,20 @@ import UIKit
 class PokemonDetailVC: UIViewController {
 
     var pokemon: Pokemon!
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var evoLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
     
-    
-    @IBOutlet var nameLbl: UILabel!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +36,11 @@ class PokemonDetailVC: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func backBtnPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
     
 
     /*
